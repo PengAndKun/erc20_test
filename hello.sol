b5d347461d6bc68dcb5add18def0531189a6492e
Kun <286768756@qq.com>
2022-05-08 07:50:19 +0800

    hello

---
+++ hello.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TestToken { 
+
+    function hello() public returns ( string memory str) {
+        return "hello world";
+    }
+}
