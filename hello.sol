19ef83f9ec40c281c4d585cf047255c557d9f52f
Kun <286768756@qq.com>
2022-05-08 08:02:42 +0800

    update return

--- hello.sol
+++ hello.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.0;
 
 contract TestToken { 
 
-    function hello() public view returns ( string memory str) {
+    function hello() public view returns ( string memory) {
         return "hello world";
     }
     
-    function add(uint256 a, uint256 b) public view returns(uint256 c){
+    function add(uint256 a, uint256 b) public view returns(uint256){
         return (a+b);
     }
 }
